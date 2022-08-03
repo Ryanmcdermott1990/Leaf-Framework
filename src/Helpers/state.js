13bc398939325e2fb0175ac93eea136201d28e03
Luke <luke@blueskylabs.com.au>
2022-08-03 20:47:15 +0800

    effect working rudimentarily

--- src/Helpers/state.js
+++ src/Helpers/state.js
@@ -1,11 +1,24 @@
-export const createState = (state, expressions, template) => {
+export const createState = (state, expressions, template, callBacks = null) => {
     return new Proxy(state, {
         set(target, property, value) {
             target[property] = value;
             refreshContent(Object.keys(state)[0], expressions, template);
+            if (callBacks && Array.isArray(callBacks)) {
+                callBacks.forEach(func => func(target[property]));
+            }
             return true;
         }
     });
+};
+
+export const addEffect = (state, callBack) => {
+    return new Proxy(state, {
+        set(target, property, value) {
+            Reflect.set(target, property, value)
+            callBack()
+            return true;
+        }
+    })
 };
 
 export const refreshContent = (state, expressions, template) => {
--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import Component from "../Component";
 import DOMNode from "../Helpers/elements";
 import Title from "./Title";
 import Button from "./Button";
-import {createState, refreshContent} from "../Helpers/state";
+import {addEffect, createState, refreshContent} from "../Helpers/state";
 
 export default function About(mountPoint, transition) {
     this.node = new DOMNode(mountPoint, transition, {
@@ -11,14 +11,11 @@ export default function About(mountPoint, transition) {
         }),
         button: new Component(Button, 200, {
             callBack: changeState, text: 'Increase counter'
-        }),
-        button2: new Component(Button, 400, {
-            callBack: alertCount, text: 'Alert Count'
         })
     });
 
     this.expressions = {
-        stateCount: () => `Count: <strong>${state.count}`
+        stateCount: () => `Count: <strong>${state?.count}`
     }
 
     let state = createState({
@@ -28,22 +25,28 @@ export default function About(mountPoint, transition) {
         this.node.element
     )
 
+    state = addEffect(state, alertCount)
+    state = addEffect(state, consoleCount)
+
     function changeState() {
         state.count = state.count + 1
     }
 
-    function alertCount() {
+    function consoleCount() {
+        console.log(state);
+    }
+
+    function alertCount(){
         alert(state.count);
     }
 
     const renderTemplate = () => {
-        const {title, button, button2} = this.node.children;
+        const {title, button} = this.node.children;
         return new Promise(async (myResolve) => {
             this.node.setHTML(`
                     <span data-UUID=${title.target}></span>
                     <p state="count" template="stateCount"></strong></p>
                     <span data-UUID=${button.target}></span>
-                    <span data-UUID=${button2.target}></span>
                 `).then(() => {
                 refreshContent('count', this.expressions, this.node.element)
                 this.node.renderChildren();
